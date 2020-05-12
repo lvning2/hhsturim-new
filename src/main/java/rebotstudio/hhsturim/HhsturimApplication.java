5100c3af8fb6f79666160b1d329902ca04351231
lvning2 <1261751961@qq.com>
2020-05-12 17:14:05 +0800

    调整样式

--- src/main/java/rebotstudio/hhsturim/HhsturimApplication.java
+++ src/main/java/rebotstudio/hhsturim/HhsturimApplication.java
@@ -1,12 +1,10 @@
 package rebotstudio.hhsturim;
 
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
-import rebotstudio.hhsturim.listener.CreateMapListener;
 
 
 @SpringBootApplication
@@ -17,7 +15,6 @@ public class HhsturimApplication {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context = SpringApplication.run(HhsturimApplication.class, args);
-        //context.addApplicationListener(new CreateMapListener());
 
 
     }
